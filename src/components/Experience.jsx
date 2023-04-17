8044271128e149f254b8ad3b4d0ca452bdcd122b
aryan-rajput1 <112474701+aryan-rajput1@users.noreply.github.com>
2023-04-17 12:18:57 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -34,12 +34,14 @@ const ExperienceCard = ({ experience }) => {
     >
       <div>
         <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
-        <p
+        <a
           className='text-secondary text-[16px] font-semibold'
           style={{ margin: 0 }}
+          href="https://www.atariyadecor.com/"
+          target="_blank"
         >
           {experience.company_name}
-        </p>
+        </a>
       </div>
 
       <ul className='mt-5 list-disc ml-5 space-y-2'>
