2518d6e9b8ea3be1069744054bf90c402cb9fcf2
aryan-rajput1 <112474701+aryan-rajput1@users.noreply.github.com>
2023-04-17 12:25:59 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -115,7 +115,8 @@ const Contact = () => {
 
           <button
             type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary 
+            hover:bg-violet-600'
           >
             {loading ? "Sending..." : "Send"}
           </button>
